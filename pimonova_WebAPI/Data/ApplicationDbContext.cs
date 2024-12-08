08be40592a064a409a28e87552508fcb7ca92d27
Антонина Пимонова <ae.pimonova@gmail.com>
2024-12-08 23:16:01 +0300

    DbContext with tables "Companies", "Users", "ObjectsOfNEI", "Workshops" and "Sectors" was added

---
+++ pimonova_WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pimonova_WebAPI.Models;
+
+namespace pimonova_WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<ObjectOfNEI> ObjectsOfNEI { get; set; }
+        public virtual DbSet<Workshop> Workshops { get; set; }
+        public virtual DbSet<Sector> Sectors { get; set; }
+
+
+    }
+}
